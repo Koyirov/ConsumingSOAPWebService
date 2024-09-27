24d0c96cfa4ac2f69811b461b4cd061b8542e399
Shohrukh Koyirov <shohrukhkoyirov@gmail.com>
2024-09-27 09:13:14 +0200

    initial commit

---
+++ src/test/java/com/example/consumingsoapwebservice/ConsumingSoapWebServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.consumingsoapwebservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsumingSoapWebServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
